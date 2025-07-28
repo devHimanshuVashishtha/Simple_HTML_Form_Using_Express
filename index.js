1d796ee29fbd8ff54c5b155686f39726ed627957
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-28 17:51:47 +0530

    Add the middleware and POST Method

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const path = require('path')
 const app = express();
 const PORT = 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/submit", (req, res) => {
+  const { name, email } = req.body;
+  res.send(`Form Submitted <b><p>name is :${name} and email is :${email}</p>`);
+});
+
 
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:3000`);``
