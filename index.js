750d788203342074570d65c111cd50953a3585ec
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-29 15:35:29 +0530

    Make connection with MongoDB Atlas

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const path = require('path')
 const app = express();
 const PORT = 3000;
+const mongoose = require('mongoose')
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const mongooseURI = "mongodb+srv://himanshuvashishtha001hp:eRluTrUEbIEv9B9a@cluster2.4rpmdkm.mongodb.net/"
+
+mongoose.connect(mongooseURI).then(()=>console.log('Database Connected'))
 
 app.post("/submit", (req, res) => {
   const { name, email } = req.body;
